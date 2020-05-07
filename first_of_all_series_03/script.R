ec0ab97b4e4f218ca83a92ed2f301a0e9f279dde
sasamori-takuya <54728859+sasamori-takuya@users.noreply.github.com>
2020-05-07 16:37:58 +0900

    Update script.R

--- first_of_all_series_03/script.R
+++ first_of_all_series_03/script.R
@@ -4,13 +4,13 @@ libs <- c("tidyverse", "ranger")
 # require and install automatically
 requireLibs <- function(libs){
   for(lib in libs){
-    if(require(lib, character.only=TRUE)){
+    if(require(lib, character.only = TRUE)){
       print(paste(lib, "is loaded correctly"))
     }
     else{
       print(paste("trying to install", lib))
       install.packages(lib)
-      if(require(lib, character.only=TRUE)){
+      if(require(lib, character.only = TRUE)){
         print(paste(lib, "installed and loaded"))
       }
       else{
