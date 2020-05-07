671a1e004192ba99530088a9303da53e00b26dee
sasamori-takuya <54728859+sasamori-takuya@users.noreply.github.com>
2020-05-07 16:36:24 +0900

    Add files via upload

---
+++ first_of_all_series_03/script.R
@@ -0,0 +1,24 @@
+# packages that we want to use
+libs <- c("tidyverse", "ranger")
+
+# require and install automatically
+requireLibs <- function(libs){
+  for(lib in libs){
+    if(require(lib, character.only=TRUE)){
+      print(paste(lib, "is loaded correctly"))
+    }
+    else{
+      print(paste("trying to install", lib))
+      install.packages(lib)
+      if(require(lib, character.only=TRUE)){
+        print(paste(lib, "installed and loaded"))
+      }
+      else{
+        stop(paste("could not install", lib))
+      }
+    }
+  }
+}
+
+# RUN
+requireLibs(libs)
